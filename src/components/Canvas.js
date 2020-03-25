fa6d5ea0a6306fb5c71a2868b99558263ceed1b9
Zymantas Katinas <kyssaz@gmail.com>
2020-03-25 15:43:06 +0200

    push title and src to array

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,20 +3,15 @@ import React, { useState, useEffect, useRef } from 'react'
 export default function Canvas(props) {
   const [drawing, setDrawing] = useState(false)
   const [restart, setRestart] = useState(true)
+  const [imgArr, setImgArr] = useState([])
+  const [title, setTitle] = useState("")
   
   const canvasRef = useRef()
   const ctx = useRef()
   const converted = useRef()
   
   useEffect(() => {
-    ctx.current = canvasRef.current.getContext('2d')
-    // ctx.current.fillStyle = "#17252A"
-    // ctx.current.fillRect(0, 0, 415, 415)
-
-    // ctx.current.strokeStyle = "#3AAFA9"
-    // ctx.current.lineWidth = 20
-    // ctx.current.strokeRect(0, 0, 415, 415)
-   
+    ctx.current = canvasRef.current.getContext('2d')   
   }, [])
 
   // Reset if reset/start is clicked in parent.
@@ -52,14 +47,12 @@ export default function Canvas(props) {
       ctx.current.stroke()
     }
   }
-
-
+ // begin path / start drawing
   function startDrawing(e) {
     ctx.current.lineJoin = 'round'
     ctx.current.lineCap = 'round'
     ctx.current.lineWidth = 20
-    // ctx.current.strokeStyle = "#3AAFA9"
-    ctx.current.strokeStyle = "white"
+    ctx.current.strokeStyle = "#3AAFA9"
     ctx.current.beginPath();
     // actual coordinates
     ctx.current.moveTo(
@@ -72,38 +65,43 @@ export default function Canvas(props) {
     ctx.current.closePath()
     // setDrawing(false)
   }
-  function handleClick(){
-    // setDrawing(true)
-    // console.log(props.title)
-  }
 
-  function download(){
+  // push img Src and Title to imgArr state onClick
+  function pushToImgArr(){
     const dataURI = canvasRef.current.toDataURL();
-    console.log(dataURI)
-    converted.current.src = dataURI
-  
-    // canvasRef.current.download = "mypainting.png";
+    setImgArr(item =>  { return[...item, { src: dataURI, title: title, id: imgArr.length }] }  )
+    console.log(imgArr)
   }
+  // map all imgArr objects ant print in div
+  const allImg = imgArr.map(item =>  <div key ={item.id}><img src={item.src} /><p>{item.title}</p></div> );
+
+  // set current title state when typing to input
+  function inputTitle(event){
+      setTitle(event.target.value)
+   }
+
   return (
     <div>
-      <button onClick ={download}> download </button>
-      <div className ="canvas"> <img ref={converted} src="" /></div>
+       <div className ="canvasImgArray">
+         {allImg}       
+        </div>
+        
+          <button onClick ={pushToImgArr}> download </button>
+          <input type="text" name="title" placeholder ="title" onChange={inputTitle} />
+      
       <div className ="canvas">
         <canvas
         ref={canvasRef}
         width={415}
         height={415}
         // onMouseDown={startDrawing}
-        onMouseOver={startDrawing}
         // onMouseUp={stopDrawing}
+        onMouseOver={startDrawing}
         onMouseOut={stopDrawing}
         onMouseMove={handleMouseMove}
-        onClick={handleClick}
         />
       </div>
     </div>
   )
 }
 
-
-//master
