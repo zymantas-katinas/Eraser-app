eb5ab6d9e5fd8c6132fa1806de19ea2927cd6626
Zymantas Katinas <kyssaz@gmail.com>
2020-03-29 15:43:08 +0300

    update

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,9 +4,6 @@ function Gallery(props){
     
     return (
         <div>
-            {/* <div className = "countdown">
-                <h1>GALLERY</h1>
-            </div> */}
             <div className ="canvasImgArray">
                 {props.allImg.slice(1)}
             </div>   
