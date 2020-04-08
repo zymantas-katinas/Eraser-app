455eb9345ac44b43001197b05f65c1267b6dc319
Zymantas Katinas <kyssaz@gmail.com>
2020-04-08 15:01:28 +0300

    Version 0.1.0

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path')
 
 require('dotenv').config();
 
@@ -9,6 +10,17 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// serve static assets if in production 
+if(process.env.NODE_ENV === "production") {
+  // set static server
+  app.use(express.static('../client/build'));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
