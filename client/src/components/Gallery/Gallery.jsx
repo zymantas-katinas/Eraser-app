6eea6a2183f292eb6254494af4ccd4ec13f382cd
Zymantas Katinas <kyssaz@gmail.com>
2020-04-09 12:31:39 +0300

    update

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -9,8 +9,8 @@ function Gallery(){
 
     // load artpieces 
     useEffect(()=> {
-        // axios.get('http://localhost:5000/artpieces/')  
-        axios.get('/artpieces/')
+        axios.get('http://localhost:5000/artpieces/')  
+        // axios.get('/artpieces/')
         .then(response => {
             setLoading(false)
             setArtpieces(response.data)  
@@ -22,8 +22,8 @@ function Gallery(){
     },[])
 
     const deleteArtpiece = (id) => {
-        // axios.delete('http://localhost:5000/artpieces/'+id)
-        axios.delete('/artpieces/'+id)
+        axios.delete('http://localhost:5000/artpieces/'+id)
+        // axios.delete('/artpieces/'+id)
         .then(response => { console.log(response.data)});
 
         setArtpieces(
